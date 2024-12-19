ed8530e8b8008c6de9e38d457991240f38da06d2
Danielcreux <jd2000_fs@hotmail.com>
2024-12-19 20:09:38 +0100

    2

---
+++ Lenguaje de Marcas/003-Manipulación de documentos web/Proyecto/Apple/033-template para los destacados/admin/escritorio.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html>
+    <head>
+        <link rel="stylesheet" href="estilo/escritorio.css">
+    </head>
+    <body>
+        <header></header>
+        <main>
+            <nav>
+                <ul>
+                    <?php include "inc/menu.php"?>
+                   
+                </ul>
+            </nav>
+            <section>
+                <table>
+                    <thead>
+                        <tr>
+                           <?php include "inc/cabeceras.php"?>
+                        </tr>
+                        
+                    </thead>
+                    <tbody>
+                       <?php include "inc/contenido.php"?>
+                    </tbody>
+                    
+                </table>
+            </section>
+        </main>
+    </body>
+
+</html>
